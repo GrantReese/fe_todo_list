07e9dcfa5126f0dfa1245bcea0d96c6803e8ab8f
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2024-02-28 20:14:10 -0600

    login working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { TodoListComponent } from './todo-list/todo-list.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [TodoListComponent],
+  imports: [RouterOutlet],
 })
 export class AppComponent {}
